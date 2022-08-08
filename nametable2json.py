18cb221253703699efd4ae90004ac7ecdb82feb3
kage1020 <73168765+kage1020@users.noreply.github.com>
2022-08-08 09:33:36 +0900

    Update nametable2json.py

--- nametable2json.py
+++ nametable2json.py
@@ -15,7 +15,7 @@ def main():
     for line in lines:
         obj['data'].append({'last':line[0], 'first':line[1], 'lastRuby':line[2], 'firstRuby':line[3]})
     
-    with open('C:/Users/yuki_/python/output/name.json', mode='w', encoding='UTF-8') as file:
+    with open('C:/Users/kage1020/python/output/name.json', mode='w', encoding='UTF-8') as file:
         json.dump(obj, file, ensure_ascii=False, indent=4)
 
 
